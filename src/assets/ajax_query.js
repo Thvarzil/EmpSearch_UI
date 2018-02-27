e3b25fd1ef90204958d20c9aa4039d1f6ec9e9bb
Louis LeBohec <louisblebohec@gmail.com>
2018-02-27 16:30:22 -0700

    moved UI over from EmpSearch_v2

---
+++ src/assets/ajax_query.js
@@ -0,0 +1,27 @@
+﻿
+
+
+$("#search_submit").on("click", function(){
+    // Clear search results display to make room for new results
+    $("#result").empty();
+
+    var search_term = $("#search_term").val();
+    console.log(search_term);
+
+    $.ajax({
+        type: 'GET',
+        url: "api/EmployeeSearch",
+        data: {},
+        contentType: "application/json; charset=utf8",
+        dataType: "json",
+        success: function(res){
+            alert(res);
+            // Takes string returned by controller, and sets display HTML to it
+            // $("#result_container").html(htmlString);
+        }
+    })
+});
+   
+
+
+
